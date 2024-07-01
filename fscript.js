cf23bae70ba34bf7d19f13e2810b1c53c2981923
divyanshnegi20 <divyanshnegi44@gmail.com>
2024-07-01 10:44:24 +0530

    Add files via upload

---
+++ fscript.js
@@ -0,0 +1,4 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('This is a demo login form. No information is being submitted.');
+});
